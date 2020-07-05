0139a5a639b6a8ab16bbc959cff3c84eef32ca9f
danij <scripty@outlook.de>
2020-07-05 12:52:38 +0200

    fixes

--- src/IndexController.js
+++ src/IndexController.js
@@ -20,8 +20,7 @@ export class IndexController {
     }
 
     async indexAction(req, res) {
-        const dirname = path.join(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
-        const view = fs.readFileSync(path.resolve(dirname, "./index.hbs"), 'utf8');
+        const view = fs.readFileSync(path.resolve("./node_modules/@scripty/server/src/index.hbs"), 'utf8');
         const template = handlebars.compile(view);
 
         let body = template({
@@ -43,3 +42,4 @@ export class IndexController {
         res.end(body);
     };
 }
+
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,24 +1,27 @@
-import path from 'path';
-
 export const registerScsDevMiddlewares = async (app) => {
 
-    const webpack = await import('webpack');
-    const webpackDevMiddleware = await import('webpack-dev-middleware');
-    const webpackHotMiddleware = await import('webpack-hot-middleware');
+    const webpackModule = await import('webpack');
+    const webpackDevMiddlewareModule = await import('webpack-dev-middleware');
+    const webpackHotMiddlewareModule = await import('webpack-hot-middleware');
 
-    const rootDir = path.resolve("./");
-    const webpackConfig = await import(rootDir + '/webpack.config.dev.cjs');
-    const compiler = webpack.default(webpackConfig.default);
+    const webpack = webpackModule.default;
+    const webpackDevMiddleware = webpackDevMiddlewareModule.default;
+    const webpackHotMiddleware = webpackHotMiddlewareModule.default;
+
+    const path = '../../../../webpack.config.dev.cjs';
+    const webpackConfig = await import(path);
+    const config = webpackConfig.default;
+    const compiler = webpack(config);
 
     app.use(
-        webpackDevMiddleware.default(compiler, {
+        webpackDevMiddleware(compiler, {
             publicPath: '/dist/',
-            watchOptions: webpackConfig.default.watchOptions,
+            watchOptions: webpackConfig.watchOptions,
             writeToDisk: false
         })
     );
 
-    app.use(webpackHotMiddleware.default(compiler));
+    app.use(webpackHotMiddleware(compiler));
 };
 
 export const ignoreFaviconMiddleware = (req, res, next) =>
